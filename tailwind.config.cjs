5a70c79157660a2bde06f50f139bc1666c6883d7
chadalen <me@chadalen.com>
2022-12-30 01:28:04 -0500

    chore: converted site to astro site

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				oleoscript: ['Oleo Script'],
-				sourcesanspro: ['Source Sans Pro']
-			},
-			colors: {
-				'deep-carrot-orange': '#f36633',
-				'japanese-indigo': '#273948',
-				'bright-gray': '#e9eef2',
-			}
-		},
-		fontFamily: {
-			sans: ['Source Sans Pro']
-		}
-	},
-	plugins: []
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        oleoscript: ['Oleo Script'],
+        sourcesanspro: ['Source Sans Pro'],
+      },
+      colors: {
+        'deep-carrot-orange': '#f36633',
+        'japanese-indigo': '#273948',
+        'bright-gray': '#e9eef2',
+      },
+    },
+    fontFamily: {
+      sans: ['Source Sans Pro'],
+    },
+  },
+  plugins: [],
 };
