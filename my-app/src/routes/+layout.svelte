d6dc352caf00238dd9658f0df767ddcf813b252b
chadalen <me@chadalen.com>
2023-04-15 03:23:06 -0400

    chore: added tailwind

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import Header from '../components/Header.svelte';
   import Footer from '../components/Footer.svelte';
+	import '../styles/styles.css';
 </script>
 
 <div class="flex flex-col h-full">
-	<Header />
+	<Header pathname='/' />
 
 	<div class="grid grid-cols-12 flex-grow">
 		<slot />
