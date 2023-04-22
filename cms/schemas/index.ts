01a6baf691a726c5721804259f3d157e19fe5a7c
chadalen <me@chadalen.com>
2023-04-22 00:47:27 -0400

    chore: added blog page

--- cms/schemas/index.ts
+++ cms/schemas/index.ts
@@ -1,5 +1,6 @@
 import about from "./about";
+import blog from "./blog";
 import hero from "./hero";
 import page from "./page";
 
-export const schemaTypes = [page, hero, about];
+export const schemaTypes = [page, hero, about, blog];
---
+++ my-app/src/routes/blog/+page.server.ts
@@ -0,0 +1,19 @@
+import { getSanityClient } from "../../utils/sanity-utils";
+
+export async function load() {
+	const client = getSanityClient();
+	const blogs = await client.fetch(
+		`*[_type == "blog"]{_id, _updatedAt, dateCreated, description, title, slug, dateCreated, excerpt, "timeToRead": round(length(content) / 5 / 180), tags}`
+	);
+
+	if (!blogs) {
+		return {
+			status: 500,
+			body: new Error('Internal Server Error')
+		};
+	}
+
+	return {
+		blogs,
+	};
+}
