b023d0335157dee7771908985aa0d84afe8fd9c5
chadalen <me@chadalen.com>
2023-05-04 22:55:29 -0400

    chore: improve spacing

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -6,9 +6,9 @@
 	class="animate__animated animate__fadeInLeft col-span-12 pt-16 pb-8 px-4 sm:py-16 md:py-0 md:col-start-2 md:col-span-6 lg:col-start-3 lg:col-span-5 2xl:col-start-4 2xl:col-span-4 flex justify-center items-center"
 >
 	<div class="text-center md:text-left">
-		<h2 class="text-2xl xl:text-4xl mb-7">{data.hero.introHeading}</h2>
+		<h2 class="text-2xl xl:text-4xl mb-4 md:mb-7">{data.hero.introHeading}</h2>
 		<h1
-			class="text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-7 text-deep-carrot-orange font-bold"
+			class="text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-4 md:mb-7 text-deep-carrot-orange font-bold"
 		>
 			{data.hero.nameHeading}
 		</h1>
