3f54fdd49d9966068441fcbb839029bf32e56806
chadalen <me@chadalen.com>
2023-08-05 11:05:03 -0400

    chore: use markdown, fix styling

--- app/src/components/Blog.svelte
+++ app/src/components/Blog.svelte
@@ -1,38 +1,70 @@
 <script lang="ts">
 	import Tag from '../components/Tag.svelte';
-	import { renderRichText, storyblokEditable, type ISbRichtext } from '@storyblok/svelte';
+	import { storyblokEditable } from '@storyblok/svelte';
+	import md from 'markdown-it';
+	import mdUtils from 'markdown-it/lib/common/utils';
+	import hljs from 'highlight.js';
+	import mila from 'markdown-it-link-attributes';
+	import '../styles/markdown.css';
+
+	const markdown = md({
+		breaks: true,
+		linkify: true,
+		highlight: function (str, lang) {
+			if (lang && hljs.getLanguage(lang)) {
+				try {
+					return (
+						'<pre class="hljs"><code>' +
+						hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+						'</code></pre>'
+					);
+				} catch (__) { /* empty */ }
+			}
+
+			return '<pre class="hljs"><code>' + mdUtils.escapeHtml(str) + '</code></pre>';
+		}
+	});
+
+	markdown.use(mila, {
+		attrs: {
+			target: '_blank',
+			rel: 'noopener'
+		}
+	});
+
 
 	type BlokData = {
 		title: string;
 		dateCreated: string;
 		tags: string[];
-		content: ISbRichtext;
+		content: string;
 	};
 
 	export let blok: BlokData;
-	$: articleHTML = renderRichText(blok.content);
+	$: blogHTML = markdown.render(blok.content);
 </script>
 
 <div use:storyblokEditable={blok} class="max-w-prose mx-auto">
-  <div class="mb-8">
-	<h1 class="text-4xl mb-4">
-		{blok.title}
-	</h1>
+	{#if blok}
+		<div class="mb-8">
+			<h1 class="text-4xl mb-4">
+				{blok.title}
+			</h1>
 
-	<div class="mb-4 text-neutral-600">
-		{blok.dateCreated &&
-			new Intl.DateTimeFormat('en-US', {
-				dateStyle: 'long'
-			}).format(new Date(blok.dateCreated))}
-	</div>
+			<div class="mb-4 text-neutral-600">
+				{blok.dateCreated &&
+					new Intl.DateTimeFormat('en-US', {
+						dateStyle: 'long'
+					}).format(new Date(blok.dateCreated))}
+			</div>
 
-	<div class="flex flex-wrap gap-4 mb-4">
-		{#each blok.tags as tag}
-			<Tag>{tag}</Tag>
-		{/each}
-	</div>
+			<div class="flex flex-wrap gap-4 mb-4">
+				{#each blok.tags as tag}
+					<Tag>{tag}</Tag>
+				{/each}
+			</div>
+		</div>
+
+		<article class="markdown" contenteditable="false" bind:innerHTML={blogHTML} />
+	{/if}
 </div>
-
-	<article class="markdown" contenteditable="false" bind:innerHTML={articleHTML} />
-</div>
-
