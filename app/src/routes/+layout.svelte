9515c9027219ac407470c4ec85d729113a20b90a
chadalen <me@chadalen.com>
2023-08-05 22:25:08 -0400

    fix: font FOUC

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,16 +2,19 @@
 	import { page } from '$app/stores';
   import Header from '../components/Header.svelte';
   import Footer from '../components/Footer.svelte';
+	import '@fontsource/oleo-script/700.css';
+	import '@fontsource/source-sans-pro/400.css';
+	import '@fontsource/source-sans-pro/700.css';
 	import '../styles/styles.css';
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
 	<title>{$page.data.seo.title}</title>
 
 	{#if $page.data.seo.description}
 		<meta name="description" content={$page.data.seo.description}>
 	{/if}
-</svelte:head>
+</svelte:head> -->
 
 <div class="flex flex-col h-full">
 	<Header />
