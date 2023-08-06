f2c9bd4ce8017e2505397660a3223c364c9541b0
chadalen <me@chadalen.com>
2023-08-06 19:04:52 -0400

    fix: blog page title

--- app/src/routes/blog/+page.ts
+++ app/src/routes/blog/+page.ts
@@ -2,15 +2,20 @@ export async function load({ parent, url }) {
 	const { storyblokApi } = await parent();
 	const page = +(url.searchParams.get('page') || '1');
 	const maxStoriesPerPage = 10;
-	const { data, headers: headersData } = await storyblokApi.get('cdn/stories', {
-		version: 'draft',
-		starts_with: 'blog/',
-		is_startpage: 0,
-		excluding_fields: 'content',
-		sort_by: 'content.dateCreated:desc',
-		page,
-		per_page: maxStoriesPerPage
-	});
+	const [{ data, headers: headersData }, { data: pageData }] = await Promise.all([
+		storyblokApi.get('cdn/stories', {
+			version: 'draft',
+			starts_with: 'blog/',
+			is_startpage: 0,
+			excluding_fields: 'content',
+			sort_by: 'content.dateCreated:desc',
+			page,
+			per_page: maxStoriesPerPage
+		}),
+		storyblokApi.get('cdn/stories/blog', {
+			version: 'draft',
+		}),
+	]);
 
 	const headers = new Headers(headersData);
 	const totalStories = +(headers.get('total') || '1');
@@ -19,8 +24,8 @@ export async function load({ parent, url }) {
 		totalPageCount: Math.ceil(totalStories / maxStoriesPerPage),
 		currentPage: page,
 		seo: {
-			title: data.story?.content?.seo?.title || '',
-			description: data.story?.content?.seo?.description || ''
+			title: pageData.story?.content?.seo?.title || 'Blogs',
+			description: pageData.story?.content?.seo?.description || ''
 		}
 	};
 }
